25a52b79a55322a6b9f54bef0a69154d6c8a4de5
MaksimKriv <max.kriv3228@gmail.com>
2023-07-27 17:29:13 +0300

    add click markers

--- src/script.js
+++ src/script.js
@@ -23,13 +23,15 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map;
+let mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (pos) {
-      console.log(pos);
       const { latitude, longitude } = pos.coords;
       const userCoord = [latitude, longitude];
-      const map = L.map("map").setView(userCoord, 13);
+      map = L.map("map").setView(userCoord, 13);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
@@ -40,9 +42,38 @@ if (navigator.geolocation) {
         .addTo(map)
         .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
         .openPopup();
+
+      map.on("click", (mapEv) => {
+        mapEvent = mapEv;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+        //console.log(lat, lng);
+      });
     },
     function () {
       alert("Вы не предоставили вашу геолокацию");
     }
   );
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    inputDistance.value = inputCadence.value = inputDuration.value = "";
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          autoClose: false,
+          className: "mark-popup",
+          closeOnClick: false,
+        })
+      )
+      .openPopup()
+      .setPopupContent("workout");
+  });
 }
+
+inputType.addEventListener("change", function (e) {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
